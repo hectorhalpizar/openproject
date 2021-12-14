b74551355cef27ae8253e778f4e80fb0df781b4a
Wieland Lindenthal <w.lindenthal@forkmerge.com>
2021-12-14 14:49:19 +0100

    [#40187] Uploaded IFC Model can't be set as Standard Model
    https://community.openproject.org/work_packages/40187

--- modules/bim/spec/features/ifc_models/ifc_upload_shared_examples.rb
+++ modules/bim/spec/features/ifc_models/ifc_upload_shared_examples.rb
@@ -2,25 +2,42 @@ shared_examples 'can upload an IFC file' do
   let(:user) { FactoryBot.create :admin }
   let(:project) { FactoryBot.create :project, enabled_module_names: %i[bim] }
   let(:ifc_fixture) { ::UploadedFile.load_from('modules/bim/spec/fixtures/files/minimal.ifc') }
+  let(:set_tick_is_default_after_file) { true }
 
   before do
     login_as user
 
     allow_any_instance_of(Bim::IfcModels::BaseContract).to receive(:ifc_attachment_is_ifc).and_return true
   end
+  
+  shared_examples 'should allow uploading an IFC file' do
+    it 'should allow uploading an IFC file' do
+      visit new_bcf_project_ifc_model_path(project_id: project.identifier)
 
-  it 'should allow uploading an IFC file' do
-    visit new_bcf_project_ifc_model_path(project_id: project.identifier)
+      page.find('#bim_ifc_models_ifc_model_is_default').set(true) if set_tick_is_default_after_file
+      page.attach_file("file", ifc_fixture.path, visible: :all)
+      page.find('#bim_ifc_models_ifc_model_is_default').set true unless set_tick_is_default_after_file
 
-    page.attach_file("file", ifc_fixture.path, visible: :all)
+      click_on "Create"
 
-    click_on "Create"
+      expect(page).to have_content("Upload succeeded")
 
-    expect(page).to have_content("Upload succeeded")
+      expect(Attachment.count).to eq 1
+      expect(Attachment.first[:file]).to eq model_name
 
-    expect(Attachment.count).to eq 1
-    expect(Attachment.first[:file]).to eq model_name
+      expect(Bim::IfcModels::IfcModel.count).to eq 1
 
-    expect(Bim::IfcModels::IfcModel.count).to eq 1
+      expect(Bim::IfcModels::IfcModel.first.is_default).to be_truthy
+    end
+  end
+
+  context 'set checkbox is_default before selecting file' do
+    let(:set_tick_is_default_after_file) { false }
+
+    it_behaves_like 'should allow uploading an IFC file'
+  end
+
+  context 'set checkbox is_default after selecting file' do
+    it_behaves_like 'should allow uploading an IFC file'
   end
 end
